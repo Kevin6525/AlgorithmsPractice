73905a24e63cd9cb3e8a93e7fe688fd6b920b714
Kevin6525 <69578702+Kevin6525@users.noreply.github.com>
2021-01-06 19:42:06 -0800

    Update #131 Palindrome Partitioning.cpp

--- #131 Palindrome Partitioning.cpp
+++ #131 Palindrome Partitioning.cpp
@@ -4,6 +4,7 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> result;
         vector<string> temp;
+        //If it's just one character size for string then push it onto the result
         if(s.size() < 2)
         {
             temp.push_back(s);
@@ -11,25 +12,31 @@ public:
             temp.pop_back();
             return result;
         }
+        //Otherwise we will search for all palindromes
         palinSearch(s, 0, result, temp);
         return result;
     }
+    // Palindrome search function
     void palinSearch(string s, int begin, vector<vector<string>> &result, vector<string> &temp)
     {
+        // Exit condition, push all the palindromes in temp into result
         if(begin >= s.size())
         {
             result.push_back(temp);
         }
+        // Iterate through if it's a palindrome push it into temp
         for(int i = begin; i < s.size(); i ++)
         {
             if(isPalin(s, begin, i))
             {
                 temp.push_back(s.substr(begin, i - begin + 1));
+                //Slice it by adding one to beginning and recursive call to search
                 palinSearch(s, i + 1, result, temp);
                 temp.pop_back();
             }
         }
     }
+    // Check if palindrome, can also just check if reverse is equal to the original
     bool isPalin(string s, int start, int end)
     {
         while(start < end)
