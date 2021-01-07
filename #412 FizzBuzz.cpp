57495b0ed57f198b07b320fc3fb5043d5e165001
Kevin6525 <69578702+Kevin6525@users.noreply.github.com>
2021-01-06 18:48:22 -0800

    Update #412 FizzBuzz.cpp

--- #412 FizzBuzz.cpp
+++ #412 FizzBuzz.cpp
@@ -1,29 +1,34 @@
+// This is an extremely simple problem that just focuses on order of checking using if/else if or switch statements
+// Whenever we have a number that is divisble by only 3, we 
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
         vector<string> result;
+        // Given the range n, use a for loop to iterate through the values from 1 to n (inclusive)
         for(int i = 1; i <= n; i++)
         {
-            if(i%3 == 0)
+            // The first condition is if it's divisible by both 3 and 5. If so, we push FizzBuzz into the vector
+            if(i % 3 == 0 && i % 5 == 0)
             {
-                if(i%5 == 0)
-                {
-                    result.push_back("FizzBuzz");
-                }
-                else
-                {
-                    result.push_back("Fizz");
-                }
+                result.push_back("FizzBuzz");
             }
-            else if(i%5 ==0)
+            // If it is only divisible by 3 and not 5, then push Fizz into the vector
+            else if(i % 3 == 0)
+            {
+                result.push_back("Fizz");
+            }
+            // If it is only divisible by 5 and not 3, then push Buzz into the vector
+            else if(i % 5 == 0)
             {
                 result.push_back("Buzz");
             }
+            // If it is neither divisble by 3 nor divisible by 5, push the integer into the vector
             else
             {
                 result.push_back(to_string(i));
             }
         }
+        // Return result vector
         return result;
     }
 };
