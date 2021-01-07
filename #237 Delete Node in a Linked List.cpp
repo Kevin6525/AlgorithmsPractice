050bdf1f60d295899d63094c79156d6408e8098f
Kevin6525 <69578702+Kevin6525@users.noreply.github.com>
2021-01-06 18:50:23 -0800

    Update #237 Delete Node in a Linked List.cpp

--- #237 Delete Node in a Linked List.cpp
+++ #237 Delete Node in a Linked List.cpp
@@ -6,6 +6,8 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// This is self explanatory, we just set the node "head" to the children
+// It isn't the actual head, but the node to be deleted
 class Solution {
 public:
     void deleteNode(ListNode* node) {
